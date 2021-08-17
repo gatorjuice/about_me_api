25a1f031da75cfb4b3639c353b11a92a4655cab8
James Gates <gatorjuice@gmail.com>
2021-08-16 21:21:25 -0500

    added first action

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
-user = User.create!(username: 'jgates', password: 'password', age: 40)
-user.favorites.create!(category: :drink, description: 'Coke')
+if Rails.env.development?
+  user = User.create!(username: 'jgates', password: 'password', age: 40)
+  user.favorites.create!(category: :drink, description: 'Coke')
+end
