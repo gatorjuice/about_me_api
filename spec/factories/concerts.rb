8479e255472b1ad6fe30a4e9fe0a802ca416f12d
James Gates <gatorjuice@gmail.com>
2021-10-17 18:36:38 -0500

    ran rubocop

--- spec/factories/concerts.rb
+++ spec/factories/concerts.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :concert do
     venue_id { 1 }
-    source_id { "MyString" }
-    band { "MyString" }
-    date_time { "2021-10-17 11:52:36" }
-    dates { "" }
-    sales { "" }
-    price_ranges { "" }
+    source_id { 'MyString' }
+    band { 'MyString' }
+    date_time { '2021-10-17 11:52:36' }
+    dates { '' }
+    sales { '' }
+    price_ranges { '' }
   end
 end
