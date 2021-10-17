df654b2a9851e3bf65dd89d5d946edd89e4a8dee
James Gates <gatorjuice@gmail.com>
2021-10-17 07:29:04 -0500

    use metadata for recordings and commented back in rescue

--- spec/apis/ticketmaster_api_client_spec.rb
+++ spec/apis/ticketmaster_api_client_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe TicketmasterApiClient do
   subject(:client) { described_class }
 
   describe '.event_search' do
-    it 'returns a list of events', vcr: { cassette_name: 'test' } do
+    it 'returns a list of events', vcr: true do
       expect(client.event_search('postalCode' => '60618')).to be_a_kind_of(Array)
     end
   end
