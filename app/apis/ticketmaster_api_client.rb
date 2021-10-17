775894b8220162c4f51770fdbe75d6ed72ed3478
James Gates <gatorjuice@gmail.com>
2021-10-17 11:15:53 -0500

    more refactor getting ready for Concert model

--- app/apis/ticketmaster_api_client.rb
+++ app/apis/ticketmaster_api_client.rb
@@ -5,25 +5,36 @@
 class TicketmasterApiClient
   BASE_URL = 'https://app.ticketmaster.com/discovery/v2/'
   INCLUDE_TEST = 'no'
+  BAND_NAME_DELIMITER = ', '
   CACHE_EXP = 10.minutes
   MAX_RESULTS = 100
 
   def self.event_search(params = {})
-    key = cache_key(__method__, query_params(params))
-    response = maybe_cached_response(key, params)
-    parsed_response = JSON.parse(response.body).dig('_embedded', 'events')
+    cached_key, success, parsed_response = execute_request(params)
 
-    if response.success? && parsed_response
+    if success && parsed_response
       parsed_response.map { |event_data| build_event(event_data) }
     else
-      maybe_log_and_clean_cache(key)
+      maybe_log_and_clean_cache(cached_key)
 
       []
     end
+  rescue StandardError => e
+    maybe_log_and_clean_cache(cached_key, e)
+
+    []
   end
 
   class << self
     private
+
+    def execute_request(params)
+      key = cache_key(__method__, query_params(params))
+      response = maybe_cached_response(key, params)
+      parsed_response = JSON.parse(response.body).dig('_embedded', 'events')
+
+      [key, response.success?, parsed_response]
+    end
 
     def maybe_cached_response(key, params)
       Rails.cache.fetch(key, expires_in: CACHE_EXP, force: params[:bust_cache]) do
@@ -47,18 +58,21 @@ class TicketmasterApiClient
     end
 
     def build_event(event_data)
-      {
-        name: event_data['name'],
-        source_id: event_data['id'],
-        dates: event_data['dates'],
-        sales: event_data['sales'],
-        price_ranges: event_data['priceRanges']
-      }
+      event_data['name'].split(BAND_NAME_DELIMITER).map do |band_name|
+        {
+          name: band_name,
+          source_id: event_data['id'],
+          dates: event_data['dates'],
+          sales: event_data['sales'],
+          price_ranges: event_data['priceRanges']
+        }
+      end
     end
 
-    def maybe_log_and_clean_cache(key)
+    def maybe_log_and_clean_cache(key, error = nil)
       return unless Rails.cache.fetch(key)
 
+      Rails.logger.error(e) if error
       Rails.logger.info("\nNo events found.\nDeleting Cached Key: #{key}\n")
       Rails.cache.delete(key)
     end
