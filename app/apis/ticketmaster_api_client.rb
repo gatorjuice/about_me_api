92fab103fbe847dcd254513bc87c15e56332ff57
James Gates <gatorjuice@gmail.com>
2021-10-17 18:34:04 -0500

    replace plain ruby hash with new concert class

--- app/apis/ticketmaster_api_client.rb
+++ app/apis/ticketmaster_api_client.rb
@@ -13,7 +13,7 @@ class TicketmasterApiClient
     cached_key, success, parsed_response = execute_request(params)
 
     if success && parsed_response
-      parsed_response.map { |event_data| build_event(event_data) }
+      parsed_response.map { |event_data| build_event(event_data) }.flatten
     else
       maybe_log_and_clean_cache(cached_key)
 
@@ -59,13 +59,13 @@ class TicketmasterApiClient
 
     def build_event(event_data)
       event_data['name'].split(BAND_NAME_DELIMITER).map do |band_name|
-        {
-          name: band_name,
+        Concert.new(
+          band: band_name,
           source_id: event_data['id'],
           dates: event_data['dates'],
           sales: event_data['sales'],
           price_ranges: event_data['priceRanges']
-        }
+        )
       end
     end
 
---
+++ db/migrate/20211017165236_create_concerts.rb
@@ -0,0 +1,15 @@
+class CreateConcerts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :concerts do |t|
+      t.integer :venue_id
+      t.string :source_id
+      t.string :band
+      t.datetime :date_time
+      t.jsonb :dates
+      t.jsonb :sales
+      t.jsonb :price_ranges
+
+      t.timestamps
+    end
+  end
+end
