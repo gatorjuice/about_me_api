5634012422ff62e42ead2c977b595f2df7f819c7
James Gates <gatorjuice@gmail.com>
2021-10-24 14:23:34 -0500

    undo cors change for a moment to get caching working

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials[Rails.env.to_sym][:allowed_origins]
+    origins '*'
 
     resource '*',
              headers: :any,
