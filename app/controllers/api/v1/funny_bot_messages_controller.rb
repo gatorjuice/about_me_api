f167a26ee28db60fd1e94e8c7f481b7985055240
Jamie <11019755+gatorjuice@users.noreply.github.com>
2021-11-01 04:21:36 -0500

    created beginnings of backend (#25)

---
+++ app/controllers/api/v1/funny_bot_messages_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class FunnyBotMessagesController < ApplicationController
+      def create
+        render_success(FunnyBotMessage.create!(funny_bot_message_params))
+      end
+
+      private
+
+      def funny_bot_message_params
+        params.permit(:user_id, :body)
+      end
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,9 @@ module AboutMeApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # ActionCable
+    config.action_cable.url = ENV.fetch('ACTION_CABLE_HOST', 'ws://localhost:3000/cable')
+    config.action_cable.disable_request_forgery_protection = true
   end
 end
