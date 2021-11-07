623242846de643c66c98f2ec1e74375d3a53b6a0
Jamie <11019755+gatorjuice@users.noreply.github.com>
2021-11-07 14:41:28 -0600

    moved seeds into categorized files (#31)

---
+++ db/seeds/funny_bot_messages.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+FunnyBotMessage.create!(user_id: 1, body: 'hello, funnybot')
---
+++ db/seeds/users.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+user = User.create!(username: 'jgates', password: 'password', age: 40)
+user.favorites.create!(category: :beverage, description: 'Coke')
