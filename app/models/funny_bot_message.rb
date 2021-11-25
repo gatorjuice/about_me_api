5ddc310dd2da4e6c5f2b8072f5df9e5d2186a525
Jamie <11019755+gatorjuice@users.noreply.github.com>
2021-11-25 10:00:00 -0500

    message should broadcast now (#45)

--- app/models/funny_bot_message.rb
+++ app/models/funny_bot_message.rb
@@ -4,6 +4,8 @@
 class FunnyBotMessage < ApplicationRecord
   after_create :broadcast_messages
 
+  belongs_to :user
+
   def broadcast_messages
     ActionCable.server.broadcast('funny_bot_channel', { funnyBotMessages: self.class.order(:created_at) })
   end
