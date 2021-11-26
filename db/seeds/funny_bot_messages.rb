ab2498e192910e20d7096fb1735f29d39186fcd7
James Gates <gatorjuice@gmail.com>
2021-11-25 19:08:25 -0600

    set messages from funny bot so they can be identified in frontend

--- db/seeds/funny_bot_messages.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-FunnyBotMessage.create!(user_id: 1, body: 'hello, funnybot')
--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -2,7 +2,8 @@
 
 users = [
   { username: 'demo_user@test.com', password: 'p@ssw@rd', age: 30 },
-  { username: 'gatorjuice@gmail.com', password: 'p@ssw@rd', age: 40 }
+  { username: 'gatorjuice@gmail.com', password: 'p@ssw@rd', age: 40 },
+  { username: 'funny_bot@about_me_api.com', password: 'p@ssw@rd', age: 1 }
 ]
 User.create!(users)
 User.last.favorites.create!(category: :beverage, description: 'Coke')
